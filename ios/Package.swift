923d3bf6683a31a87bdcc1ee5511c4118d67bd25
Daniel DeKerlegand <dldekerl@gmail.com>
2026-08-20 02:14:17 -0500

    feat: [US-1] - A concrete SQLite-backed UploadStore

--- ios/Package.swift
+++ ios/Package.swift
@@ -22,6 +22,10 @@ let package = Package(
         .library(name: "Amphora", targets: ["Amphora"]),
     ],
     targets: [
-        .target(name: "Amphora", path: "Sources/Amphora"),
+        .target(
+            name: "Amphora",
+            path: "Sources/Amphora",
+            linkerSettings: [.linkedLibrary("sqlite3")]
+        ),
     ]
 )
